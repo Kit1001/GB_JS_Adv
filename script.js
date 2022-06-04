2327dd30edd90cb3a67f14da77483c1fc9448c0e
Dmitry Serov <eitelkeit999@gmail.com>
2022-06-04 22:27:07 +0400

    added class for single Good

--- script.js
+++ script.js
@@ -1,49 +1,69 @@
 const prefetchedGoods = [
-  {title: 'Shirt', price: 150, img: 'shirt.jpeg'},
-  {title: 'Socks', price: 50, img: 'socks.jpeg'},
-  {title: 'Jacket', price: 350, img: 'jacket.jpg'},
-  {title: 'Shoes', price: 250, img: 'shoes.jpeg'},
+  {id: 1, title: 'Shirt', price: 150, img: 'shirt.jpeg'},
+  {id: 2, title: 'Shoes', price: 250, img: 'shoes.jpeg'},
+  {id: 3, title: 'Socks', price: 50, img: 'socks.jpeg'},
+  {id: 4, title: 'Jacket', price: 350, img: 'jacket.jpg'},
 ];
+
+class Good {
+  constructor(item) {
+    this.id = item.id;
+    this.title = item.title;
+    this.price = item.price;
+    this.img = item.img;
+  }
+
+  createMarkDown = () =>
+    `<div class="goods-item">
+    <img src="imgs/${this.img}" alt="good's image"/>
+    <h3>${this.title}</h3>
+    <p>${this.price}</p>
+    </div>`
+
+}
 
 class GoodsList {
   static goods = [];
   static container = '';
 
   static fetchGoods(goods) {
-    this.goods = goods;
+    for (const good of goods) {
+      this.goods.push(new Good(good))
+    }
   }
 
-  static renderItem = props =>
-    `<div class="goods-item">
-    <img src="imgs/${props.img}" alt="good's image"/>
-    <h3>${props.title}</h3>
-    <p>${props.price}</p>
-  </div>`
+  static render = () => document.querySelector(this.container).innerHTML =
+    this.goods.map(item => item.createMarkDown()).join('\n')
 
-  static renderList = () => document.querySelector(this.container).innerHTML =
-    this.goods.map(item => this.renderItem(item)).join('\n');
-
-  static goodsPriceSum = () => this.goods.reduce((acc, obj) => acc + obj.price, 0);
+  static
+  goodsPriceSum = () => this.goods.reduce((acc, obj) => acc + obj.price, 0);
 }
 
 
-class Basket{
+class Basket {
   static cart;
 
-  static render(){};
-  static getSumOfCart(){};
+  static render() {
+  };
+
+  static getSumOfCart() {
+  };
 }
 
-class BasketItem{
+class BasketItem {
   constructor(goodId) {
-    this.Id = goodId;
+    this.id = goodId;
   }
 
-  addToCart(){};
-  removeFromCart(){};
+  addToCart() {
+  };
+
+  removeFromCart() {
+  };
 }
+
 
 GoodsList.fetchGoods(prefetchedGoods);  // берем список товаров из prefetched goods
 GoodsList.container = '.goods-list';  // задаем название контейнера для добавления в него новой разметки
-GoodsList.renderList();  // добавляем созданную разметку на страницу
+GoodsList.render();  // добавляем созданную разметку на страницу
 console.log(GoodsList.goodsPriceSum());  // вывод в консоль суммы цен товаров
